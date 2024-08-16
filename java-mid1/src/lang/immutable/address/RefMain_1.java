9a1e811dfe5a0b1b3e7966ca39447ca20b323140
Lee Ji Hyeok <illu140411@gmail.com>
2024-08-16 19:47:31 +0900

    자바 중급 연습

---
+++ java-mid1/src/lang/immutable/address/RefMain_1.java
@@ -0,0 +1,15 @@
+package lang.immutable.address;
+
+public class RefMain_1 {
+    public static void main(String[] args) {
+        Address a = new Address("서울");
+        Address b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        b.setValue("부산");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
