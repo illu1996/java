b5acd34eafd736beba4afb0c3991cb2a0a871b6b
Jerry <illu140411@gmail.com>
2025-04-01 21:15:57 +0900

    spring day1

---
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    //null 대신 Optional 로 반환
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
