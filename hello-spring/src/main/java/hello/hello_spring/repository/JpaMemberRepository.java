ef6e48602697da6accfc1799dd2526ce33fdb65d
Jerry <illu140411@gmail.com>
2025-04-09 18:46:10 +0900

    spring day5 JPA

---
+++ hello-spring/src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    // JPA 를 사용하려면 DB에 @Tranjactional 이 필수이다.
+    // 그러므로 MemberService에 Tranjactional 을 넣어주어야 한다.
+    // 회원가입만 DB에 넣기에 회원가입만 넣어도 된다.
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList().stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+}
